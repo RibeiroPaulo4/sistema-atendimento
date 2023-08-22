8627aceaa921e786a67c462d0be8aa0adf93b9ed
Paulo Ribeiro <sribeirocontador@gmail.com>
2023-08-21 22:18:49 -0400

    selecionando elementos do DOM

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 import "./libs"
+const  inputNome = document.querySelector('#input-nome')
+const  selectPrioridade = document.querySelector('#select-prioridade')
+const btnIncluir = document.querySelector('#btn-incluir')
+const ullista = document.querySelector('#ul-lista')
+const displayNome = document.querySelector('#display-nome')
+const btnChamar = document.querySelector('#btn-chamar')
+const listaDeAtendimento = new Array()
+
